e3c2f241d7d35fdc5e517cb7a18d13ed42b3f5a1
Lissette <li.arenas@duocuc.cl>
2022-10-17 21:38:33 -0300

    form reg y splash

--- src/app/component/formulario-registro/formulario-registro.component.ts
+++ src/app/component/formulario-registro/formulario-registro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms"
+import { Router } from '@angular/router';
+import { LocalstorageService } from 'src/app/services/data/localstorage.service';
+
 
 @Component({
   selector: 'app-formulario-registro-comp',
@@ -7,8 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioRegistroComponent implements OnInit {
 
-  constructor() { }
+  registroform:FormGroup;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private localstorage:LocalstorageService,
+    private router:Router
+  ) { 
+    this.registroform= this.formbuilder.group({
+      nombreUsuario: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])),
+
+      apellidoUsuario: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(3)
+      ])),
+
+      rutUsuario: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(9)
+      ])),
+
+      emailUsuario: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(9)
+      ])),
+
+      telefonoUsuario: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(10)
+      ])),
+
+      fecnacUsuario: new FormControl("", Validators.compose([
+        Validators.required
+      ])),
+
+      patenteUsuario: new FormControl("", Validators.compose([
+        Validators.required
+      ]))
+  });
+  }
+
+  get nombreUsuario() { return this.registroform.get('nombreUsuario');}
+  get apellidoUsuario() { return this.registroform.get('apellidoUsuario');}
+  get rutUsuario() { return this.registroform.get('rutUsuario');}
+  get emailUsuario() { return this.registroform.get('emailUsuario');}
+  get telefonoUsuario() { return this.registroform.get('telefonoUsuario');}
+  get fecnacUsuario() { return this.registroform.get('fecnacUsuario');}
+  get patenteUsuario() { return this.registroform.get('patenteUsuario');}
+
 
   ngOnInit() {}
-
+    datos(registros){
+      console.log(registros.nombreUsuario);
+  }
 }
--- src/app/page/formulario-registro/formulario-registro.module.ts
+++ src/app/page/formulario-registro/formulario-registro.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,15 +8,19 @@ import { FormularioRegistroPageRoutingModule } from './formulario-registro-routi
 
 import { FormularioRegistroPage } from './formulario-registro.page';
 import { FormularioRegistroComponent } from 'src/app/component/formulario-registro/formulario-registro.component';
+import { FooterComponent } from 'src/app/component/footer/footer.component';
 import { BaseModule } from 'src/app/component/base.module';
+
+
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
     FormularioRegistroPageRoutingModule,
-    BaseModule
+    BaseModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   declarations: [FormularioRegistroPage,FormularioRegistroComponent]
 })
